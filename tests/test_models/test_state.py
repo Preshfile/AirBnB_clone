3b51c1af8008ae62b5ecb8e6d7a6a385111c45d9
Deborah Thomas <mzdamidee@gmail.com>
2022-11-24 22:14:51 +0100

    Test model added

--- tests/test_models/test_state.py
+++ tests/test_models/test_state.py
@@ -1,61 +1,23 @@
 #!/usr/bin/python3
-"""Unittest for BaseModel"""
-import os
-import time
+"""Test suite for the State class of the models.state module"""
 import unittest
-from datetime import datetime
-from models import storage
+
+from models.base_model import BaseModel
 from models.state import State
 
 
 class TestState(unittest.TestCase):
-    """test BaseModel"""
+    """Test cases for the State class"""
 
-    def test_init(self):
-        """test blank basemodel init"""
-        snapshot = datetime.now()
-        sm1 = State()
-        snapshot2 = datetime.now()
+    def setUp(self):
+        self.state = State()
 
-        self.assertIsInstance(sm1.id, str)
-        self.assertTrue(len(sm1.id) > 0)
-        self.assertTrue('State.' + sm1.id in storage.all().keys())
+    def test_state_is_a_subclass_of_basemodel(self):
+        self.assertTrue(issubclass(type(self.state), BaseModel))
 
-        self.assertIsInstance(sm1.created_at, datetime)
-        self.assertLess(sm1.created_at, snapshot2)
-        self.assertGreater(sm1.created_at, snapshot)
-        
-        self.assertIsInstance(sm1.updated_at, datetime)
-        self.assertLess(sm1.updated_at, snapshot2)
-        self.assertGreater(sm1.updated_at, snapshot)
-        
-        sm1.save()
-        self.assertIsInstance(sm1.updated_at, datetime)
-        self.assertGreater(sm1.updated_at, snapshot)
-        self.assertGreater(sm1.updated_at, snapshot2)
-        del sm1
-        
-    def test_init_dict(self):
-        """test dict basemodel init"""
-        test_dict = {'updated_at': datetime(1963, 11, 22, 12, 30, 00, 716921).isoformat('T')
-                     , 'id': 'z3854b62-93fa-fbbe-27de-630706f8313c', 'created_at': datetime(1963, 11, 22, 12, 30, 00, 716921).isoformat('T')}
-        sm2 = State(**test_dict)
+    def test_attr_is_a_class_attr(self):
+        self.assertTrue(hasattr(self.state, "name"))
 
-        self.assertIsInstance(sm2.id, str)
-        self.assertTrue(len(sm2.id) > 0)
-        self.assertTrue(sm2.id == test_dict['id'])
-        
-        self.assertIsInstance(sm2.created_at, datetime)
-        self.assertTrue(sm2.created_at.isoformat('T') == test_dict['created_at'])
-        self.assertIsInstance(sm2.updated_at, datetime)
-        self.assertTrue(sm2.updated_at.isoformat('T') == test_dict['updated_at'])
-        sm2.save()
-        self.assertGreater(sm2.updated_at, sm2.created_at)
-        del sm2
-
-    def test_attribute(self):
-        """asdad"""
-        sm3 = State()
-
-        self.assertTrue(hasattr(sm3, "name"))
-        self.assertIsInstance(sm3.name, str)
+    def test_class_attrs(self):
+        self.assertIs(type(self.state.name), str)
+        self.assertFalse(bool(self.state.name))
--- tests/test_models/test_user.py
+++ tests/test_models/test_user.py
@@ -1,68 +1,27 @@
 #!/usr/bin/python3
-"""Unittest for BaseModel"""
-import os
-import time
+"""Test suite for the User class in models.user"""
 import unittest
-from datetime import datetime
-from models import storage
+from models.base_model import BaseModel
+
 from models.user import User
 
 
 class TestUser(unittest.TestCase):
-    """test BaseModel"""
+    """Test cases against the User class"""
 
-    def test_ainit(self):
-        """test blank basemodel init"""
-        snapshot = datetime.now()
-        um1 = User()
-        snapshot2 = datetime.now()
+    def test_attrs_are_class_attrs(self):
+        u = User()
+        # test that it is a class attribute
+        self.assertTrue(hasattr(User, "first_name")
+                        and hasattr(User, "last_name"))
 
-        self.assertIsInstance(um1.id, str)
-        self.assertTrue(len(um1.id) > 0)
-        self.assertTrue('User.' + um1.id in storage.all().keys())
+    def test_class_attrs(self):
+        u = User()
+        self.assertIs(type(u.first_name), str)
+        self.assertIs(type(u.last_name), str)
+        self.assertTrue(u.first_name == "")
+        self.assertTrue(u.last_name == "")
 
-        self.assertIsInstance(um1.created_at, datetime)
-        self.assertLess(um1.created_at, snapshot2)
-        self.assertGreater(um1.created_at, snapshot)
-        
-        self.assertIsInstance(um1.updated_at, datetime)
-        self.assertLess(um1.updated_at, snapshot2)
-        self.assertGreater(um1.updated_at, snapshot)
-        
-        um1.save()
-        self.assertIsInstance(um1.updated_at, datetime)
-        self.assertGreater(um1.updated_at, snapshot)
-        self.assertGreater(um1.updated_at, snapshot2)
-        del um1
-        
-    def test_init_dict(self):
-        """test dict basemodel init"""
-        test_dict = {'updated_at': datetime(1963, 11, 22, 12, 30, 00, 716921).isoformat('T')
-                     , 'id': 'z3854b62-93fa-fbbe-27de-630706f8313c', 'created_at': datetime(1963, 11, 22, 12, 30, 00, 716921).isoformat('T')}
-        um2 = User(**test_dict)
-
-        self.assertIsInstance(um2.id, str)
-        self.assertTrue(len(um2.id) > 0)
-        self.assertTrue(um2.id == test_dict['id'])
-        
-        self.assertIsInstance(um2.created_at, datetime)
-        self.assertTrue(um2.created_at.isoformat('T') == test_dict['created_at'])
-        self.assertIsInstance(um2.updated_at, datetime)
-        self.assertTrue(um2.updated_at.isoformat('T') == test_dict['updated_at'])
-        um2.save()
-        self.assertGreater(um2.updated_at, um2.created_at)
-        del um2
-
-    def test_attribute(self):
-        """asdad"""
-        um3 = User()
-
-        self.assertTrue(hasattr(um3, "email"))
-        self.assertTrue(hasattr(um3, "password"))
-        self.assertTrue(hasattr(um3, "first_name"))
-        self.assertTrue(hasattr(um3, "last_name"))
-
-        self.assertIsInstance(um3.email, str)
-        self.assertIsInstance(um3.password, str)
-        self.assertIsInstance(um3.first_name, str)
-        self.assertIsInstance(um3.last_name, str)
+    def test_user_is_a_subclass_of_basemodel(self):
+        u = User()
+        self.assertTrue(issubclass(type(u), BaseModel))
